84949ba096cc536b9976ea0204992fd0897bb65e
Carlos <hernandezc2@ufl.edu>
2026-02-17 18:34:41 -0500

    added sim id

--- src/test_parquet.py
+++ src/test_parquet.py
@@ -6,14 +6,20 @@ import pyarrow as pa
 def main():
     parquet_file = "test_pendulum.parquet"
     writer = None
-    count = 0
+    num_sims = 0
+    max_theta1 = 10
+    max_theta2 = 20
+    step = 0.1
 
     # SMALL TEST GRID
-    for init_theta1 in np.arange(0, 10, 5):      # 0, 5 degrees
-        for init_theta2 in np.arange(0, 20, 10): # 0, 10 degrees
-            
+    for init_theta1 in np.arange(0, max_theta1, step):      # 0, 5 degrees
+        for init_theta2 in np.arange(0, max_theta2, step):  # 0, 10 degrees
+            sim_id = int(round(init_theta1 / step)) * int(max_theta2 / step) + int(round(init_theta2 / step))
+
             pendulum = DoublePendulum(init_theta1, init_theta2)
             df = pendulum.generateTimeData()
+            df["sim_id"] = sim_id
+            
 
             table = pa.Table.from_pandas(df)
 
@@ -21,14 +27,14 @@ def main():
                 writer = pq.ParquetWriter(parquet_file, table.schema)
 
             writer.write_table(table)
-            count += 1
+            num_sims += 1
 
-            print(f"Simulation {count} complete")
+            print(f"Simulation {sim_id} complete")
 
     if writer:
         writer.close()
 
-    print(f"\nSaved {count} simulations to {parquet_file}")
+    print(f"\nSaved {num_sims} simulations to {parquet_file}")
 
 if __name__ == "__main__":
     main()
